ad78dcd8aef38b24c5d052d1ef38cf74cd152746
JaquelineSantos11 <jaqueribsantos@gmail.com>
2018-05-04 20:20:20 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,4 +12,8 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
+    
+class Candidato(models.Model):
+    nome = models.CharField(max_length=200)
+
 
